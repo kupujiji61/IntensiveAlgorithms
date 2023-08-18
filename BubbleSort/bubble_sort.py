15650556c91d26c614a44a48f4e34871ef76d5ca
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2023-08-18 18:20:27 +0300

    Update bubble_sort.py

--- BubbleSort/bubble_sort.py
+++ BubbleSort/bubble_sort.py
@@ -1,18 +1,12 @@
 from typing import List
 
-
 def bubble_sort(array: List[int]) -> List[int]:
-    """
-    Sorts the array in ascending order using bubble sort
-
-    Args:
-        array (List[int]): input
-
-    Returns:
-        List[int]: sorted array
-    """
-
-    return []
+    n = len(array)
+    for i in range(n):
+        for i in range(n - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+    return array
 
 
 # https://leetcode.com/problems/sort-colors/description/
