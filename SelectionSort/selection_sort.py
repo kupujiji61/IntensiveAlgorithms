d2344b60ed6a9d4cb9eba82a7eb820288511980d
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2023-08-18 18:32:20 +0300

    Update selection_sort.py

--- SelectionSort/selection_sort.py
+++ SelectionSort/selection_sort.py
@@ -1,18 +1,24 @@
 from typing import List
 
 
+from typing import List
+
+
 def selection_sort(array: List[int]) -> List[int]:
-    """
-    Sorts the array in ascending order using selection sort
+    n = len(array)
+    
+    for i in range(0, n - 1):
+        current_minimum = i + 1
+        for j in range(i + 1, n):
+            if array[j] < array[current_minimum]:
+                current_minimum = j
+        if current_minimum < array[i]:
+            array[i], array[current_minimum] = array[current_minimum], array[i]
+    
+    return array
+            
 
-    Args:
-        array (List[int]): input
-
-    Returns:
-        List[int]: sorted array
-    """
-
-    return []
+# https://leetcode.com/problems/sort-colors/
 
 
 # https://leetcode.com/problems/sort-colors/
